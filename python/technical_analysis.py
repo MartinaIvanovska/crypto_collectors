634290b8d8446a8b3c5ef35cb7c827c5cee9708c
Mila Spasevska <mila.spasevska@students.finki.ukim.mk>
2025-12-13 14:48:28 +0100

    add and test oscillators rsi, macd and stoch

--- python/technical_analysis.py
+++ python/technical_analysis.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import pandas as pd
+import ta
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "..", "data", "crypto_daily.db")
@@ -21,11 +22,28 @@ def load_data():
     df = df.sort_values(["symbol", "date"])
     return df
 
+def add_indicators(df):
+    """
+    First version: add oscillators rsi, macd, stoch
+    """
+    if len(df) == 0:
+        return df
+
+    df["rsi"] = ta.momentum.rsi(df["close"], window = 14)
+    df["macd"] = ta.trend.macd_diff(df["close"])
+    df["stoch"] = ta.momentum.stoch(df["high"], df["low"], df["close"])
+    return df
+
+
 def compute_for_all():
     df = load_data()
     for symbol, g in df.groupby("symbol"):
+        g = g.copy();
+        g.set_index("date", inplace=True)
+
+        daily = add_indicators(g.copy())
         print("\nSymbol:", symbol)
-        print(g.head(3))
+        print(daily[["close", "rsi", "macd", "stoch"]].tail(3))
 
 
 def main():
